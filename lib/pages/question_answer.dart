474a9aaaaa050d79a66b18314b7ea91ebdefc4e6
Abrar Mahi <amahi2001@gmail.com>
2022-04-09 19:20:12 -0400

    testing authentication/ firebase integration

--- lib/pages/question_answer.dart
+++ lib/pages/question_answer.dart
@@ -1,9 +1,31 @@
 import 'package:flutter/material.dart';
 import 'package:http/http.dart' as http;
+import 'package:firebase_core/firebase_core.dart';
+import 'package:firebase_auth/firebase_auth.dart';
 
 class QuestionAnswerPage extends StatefulWidget {
   @override
   State<QuestionAnswerPage> createState() => _QuestionAnswerPageState();
+}
+
+Future<void> signUp() async {
+  await Firebase.initializeApp();
+
+  try {
+    await FirebaseAuth.instance.createUserWithEmailAndPassword(
+      email: 'amahi2001@gmail.com',
+      password: '12345678',
+    );
+    print('Successfully signed up');
+  } on FirebaseAuthException catch (e) {
+    if (e.code == 'weak-password') {
+      print('The password provided is too weak.');
+    } else if (e.code == 'email-already-in-use') {
+      print('The account already exists for that email.');
+    }
+  } catch (e) {
+    print(e.toString());
+  }
 }
 
 class _QuestionAnswerPageState extends State<QuestionAnswerPage> {
@@ -20,33 +42,15 @@ class _QuestionAnswerPageState extends State<QuestionAnswerPage> {
         elevation: 0, //shadow below the appBar
         backgroundColor: Colors.orange[600],
       ),
-      body: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          crossAxisAlignment: CrossAxisAlignment.center,
-          children: [
-            Container(
-              width: 0.5 * MediaQuery.of(context).size.width,
-              child: TextField(
-                decoration: InputDecoration(
-                    labelText: 'Ask a Yes/No Question',
-                    border: OutlineInputBorder()),
-              ),
-            ),
-            SizedBox(height: 20),
-            Row(
-                mainAxisAlignment: MainAxisAlignment.center,
-                crossAxisAlignment: CrossAxisAlignment.center,
-                children: [
-                  ElevatedButton(onPressed: () {}, child: Text('Get Answer')),
-                  SizedBox(
-                    width: 20,
-                  ),
-                  ElevatedButton(
-                    onPressed: () {},
-                    child: Text('Reset'),
-                  ),
-                ])
-          ]),
+      body: ListView(
+        children: [
+          ElevatedButton(
+              onPressed: () async {
+                await signUp();
+              },
+              child: Text('Sign Up')),
+        ],
+      ),
     );
   }
 }
